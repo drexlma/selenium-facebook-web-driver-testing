8be864478966efec8df26c0ebbc4a9d32604c56d
ddrexlmaier <>
2018-02-18 12:31:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# selenium-facebook-web-driver-testing
---
+++ run.sh
@@ -0,0 +1,10 @@
+mkdir ~/.vnc/
+echo "secret" > ~/.vnc/passwd
+
+
+docker stop $(docker ps -q --filter ancestor=drexlmaier/selenium-facebook-web-driver-testing )
+docker build -t drexlmaier/selenium-facebook-web-driver-testing .
+docker run  -p 4444:4444 --security-opt seccomp:unconfined -d drexlmaier/selenium-facebook-web-driver-testing
+
+docker exec -it $(docker ps -q --filter ancestor=drexlmaier/selenium-facebook-web-driver-testing ) bash
+
